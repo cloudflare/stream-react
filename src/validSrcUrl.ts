31bca2fe353768cd1d53a8e24285f333a5523fd9
Javier Martínez <javi@javimartinez.es>
2022-04-25 18:14:54 +0200

    Allow cloudflarestream.com URLs too (#65)

--- src/validSrcUrl.ts
+++ src/validSrcUrl.ts
@@ -1,7 +1,7 @@
 export function validSrcUrl(str: string) {
   try {
     const url = new URL(str);
-    return url.hostname.endsWith("videodelivery.net");
+    return url.hostname.endsWith("videodelivery.net") || url.hostname.endsWith("cloudflarestream.com");
   } catch {
     return false;
   }

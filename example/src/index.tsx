b4555216c71c2fc8682fec8fc2f82eeae3e049c6
Kevin Kipp <kevin.kipp@gmail.com>
2023-12-04 12:10:50 -0600

    Update local example

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,7 +1,6 @@
-import "react-app-polyfill/ie11";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Stream, StreamPlayerApi } from "../.";
+import { Stream, StreamPlayerApi } from "../..";
 import { useState } from "react";
 
 function useLocalStorageState<T>(key: string, defaultValue: T) {
